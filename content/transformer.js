4937853c55d63e56bb4494bf336cca09b8fc0524
Lomuzord <lomuzord@users.noreply.github.com>
2026-03-22 09:18:34 +0100

    feat: restore extension logo in sidepanel and overlay header

--- content/transformer.js
+++ content/transformer.js
@@ -1134,7 +1134,9 @@
       <a href="#ua-main-content" class="ua-skip-link">Skip to content</a>
       <header class="ua-overlay-header">
         <div class="ua-header-left">
-          <span class="ua-logo" aria-hidden="true">${SVG_WHEELCHAIR}</span>
+          <span class="ua-logo" aria-hidden="true">
+            <img class="ua-icon" src="${chrome.runtime.getURL('icons/icon-on.svg')}" alt="">
+          </span>
           <span class="ua-header-title">Universal Access</span>
           <span class="ua-type-badge">${esc(type)}</span>
         </div>
